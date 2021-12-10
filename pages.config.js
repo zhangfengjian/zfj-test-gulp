a418910c7b6417bb2defe8a23958319e9f394712
zhangfengjian <1213675330@qq.com>
2021-12-10 15:46:27 +0800

    删除gulpfile使用zfj-gulp指令打包

---
+++ pages.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  data: {
+    "menus": [
+      {
+        "name": "Home11",
+        "icon": "aperture",
+        "link": "index.html"
+      },
+      {
+        "name": "Features",
+        "link": "features.html"
+      },
+      {
+        "name": "About",
+        "link": "about.html"
+      },
+      {
+        "name": "Contact",
+        "link": "#",
+        "children": [
+          {
+            "name": "Twitter",
+            "link": "https: //twitter.com/w_zce"
+          },
+          {
+            "name": "About",
+            "link": "https: //weibo.com/zceme"
+          },
+          {
+            "name": "divider"
+          },
+          {
+            "name": "About",
+            "link": "https: //github.com/zce"
+          }
+        ]
+      }
+    ],
+    "pkg": require('./package.json'),
+    "date": "2021-12-07T09:46:25.106Z"
+  } 
+}
